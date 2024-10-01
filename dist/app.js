b5d2c01bf005c6d38ae9f10cc15567a62da80c38
zaksiu12s <zaksiu279@gmail.com>
2024-10-02 00:02:06 +0200

    Other initializations

--- dist/app.js
+++ dist/app.js
@@ -4,9 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 require("dotenv/config");
 const app = (0, express_1.default)();
 const PORT = Number(process.env.PORT) || 4000;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+// app.get("/", (req: Request, res: Response) => {
+//   res.status(200).send("Server running");
+// });
 app.listen(PORT, () => {
     console.log(`Server running on: ${PORT}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
-import express, { Express } from "express";
-import bodyParser from "body-parser";
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 
@@ -7,7 +6,12 @@ const app: Express = express();
 const PORT: Number = Number(process.env.PORT) || 4000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.status(200).send("Server running");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on: ${PORT}`);
