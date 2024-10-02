4f20c060fc6088d31279fc5374719627f9369a8f
zaksiu12s <zaksiu279@gmail.com>
2024-10-02 17:48:23 +0200

    Added server.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,14 @@
-import express, { Express, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import "dotenv/config";
 
-const app: Express = express();
-const PORT: Number = Number(process.env.PORT) || 4000;
+const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.get("/", (req: Request, res: Response) => {
-//   res.status(200).send("Server running");
-// });
-
-app.listen(PORT, () => {
-  console.log(`Server running on: ${PORT}`);
+app.get('/', (req: Request, res: Response) => {
+  res.send('API running...');
 });
+
+export default app;
