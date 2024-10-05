d699729df8d3a74c8b34c8f7ca53b7349b350f4f
zaksiu12s <zaksiu279@gmail.com>
2024-10-05 18:05:04 +0200

    Better comments

--- dist/controllers/userController.js
+++ dist/controllers/userController.js
@@ -5,133 +5,131 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUser = exports.createUser = exports.getUser = void 0;
 const bcrypt_1 = __importDefault(require("bcrypt"));
+//Importing user model and interface for mongoose validation
 const userModel_1 = __importDefault(require("../models/userModel"));
+// Importing HTTP status codes for response status codes
 const statusCodes_1 = require("../config/statusCodes");
+// Function used to get user data from the database with email | name and password
 const getUser = async (req, res) => {
+    // Casting body as IGetUserValidatorSchema interface and writing data to variables
+    // userName or userEmail will be provided
     const body = req.body;
     const userName = body.name;
     const userEmail = body.email;
     const userPassword = body.password;
     try {
+        // Searches user by name or email and returns the user if found
         const userData = await searchUser(userName, userEmail);
         if (!userData) {
             res.status(statusCodes_1.HttpStatusCode.NOT_FOUND).send('User not found');
             return;
         }
-        // checks if the password is correct
+        // Checks if the password is correct
         const isMatch = await bcrypt_1.default.compare(userPassword, userData.password);
         if (!isMatch) {
-            res
-                .status(statusCodes_1.HttpStatusCode.UNAUTHORIZED)
-                .send('Incorrect password');
+            res.status(statusCodes_1.HttpStatusCode.UNAUTHORIZED).send('Incorrect password');
             return;
         }
+        // Creates a new userResponseData with user data that will be sent to the client
         const userResponseData = {
             name: userData.name,
             email: userData.email,
             signInDate: userData.signInDate,
         };
-        res
-            .status(statusCodes_1.HttpStatusCode.OK)
-            .json(userResponseData);
+        res.status(statusCodes_1.HttpStatusCode.OK).json(userResponseData);
     }
     catch (error) {
-        res
-            .status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER)
-            .send('Internal server error');
+        // The errors caught during the searchUser execution (mongodb errors) or bcrypt comparison 
+        console.log(error);
+        res.status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER).send('Internal server error');
     }
 };
 exports.getUser = getUser;
+// Creates a new user with the specified data email, name and password if the user does not exist 
+// (name and email must not be in the db)
 const createUser = async (req, res) => {
-    // save the request body to body variable with correct types 
+    // Casting body as ICreateUserValidatorSchema interface and writing data to variables
     const body = req.body;
     const userName = body.name;
     const userEmail = body.email;
     const userPassword = body.password;
-    // searches the database for the user
+    // Searches for user with the same email or name, if found responds to client
     try {
-        // if user was found then responds with conflict status and returns
         if (await searchUser(userName, userEmail)) {
-            res
-                .status(statusCodes_1.HttpStatusCode.CONFLICT)
-                .json({ message: 'User already exists' });
+            res.status(statusCodes_1.HttpStatusCode.CONFLICT).json({ message: 'User already exists' });
             return;
         }
     }
     catch (error) {
+        // Mongodb internal errors
         console.log(error);
-        res
-            .status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER)
-            .json({ message: 'MongoDB error' });
+        res.status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER).json({ message: 'MongoDB error' });
         return;
     }
-    // the hashing for the password
+    // The hashing for the password
     let hash;
     try {
-        // tries to hash the password using bcrypt
         hash = await bcrypt_1.default.hash(userPassword, 10);
     }
     catch (error) {
+        // Hashing password errors
         console.log(error);
-        res
-            .status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER)
-            .json({ message: 'Hashing failed' });
+        res.status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER).json({ message: 'Hashing failed' });
         return;
     }
-    // creates new user with hashed password and saves it to the database
+    // Creates new user with hashed password 
     const user = new userModel_1.default({
         name: userName,
         email: userEmail,
         password: hash,
         signInDate: new Date(),
     });
+    // Saving user to mongoDB database
     try {
         await user.save();
+        res.status(statusCodes_1.HttpStatusCode.OK).send(`User with name: ${userName} and email: ${userEmail} created successfully.`);
     }
     catch (error) {
+        // MongoDB internal errors
         console.log(error);
-        res
-            .status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER)
-            .json({ message: 'User already exists' });
+        res.status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER).json({ message: 'MongoDB error' });
         return;
     }
-    res
-        .status(statusCodes_1.HttpStatusCode.OK)
-        .send(`User with name: ${userName}, email: ${userEmail}, password: ${userPassword} created successfully.`);
 };
 exports.createUser = createUser;
+// Removes user with the specified email | name and password if exists
 const deleteUser = async (req, res) => {
+    // Casting body as IGetUserValidatorSchema interface and writing data to variables
     const body = req.body;
     const userName = body.name;
     const userEmail = body.email;
     const userPassword = body.password;
     try {
+        // Searching for user in the db with userName or userEmail
         const userData = await searchUser(userName, userEmail);
         if (!userData) {
             res.status(statusCodes_1.HttpStatusCode.NOT_FOUND).send('User not found');
             return;
         }
-        // checks if the password is correct
+        // Checking if the password is correct
         const isMatch = await bcrypt_1.default.compare(userPassword, userData.password);
         if (!isMatch) {
-            res
-                .status(statusCodes_1.HttpStatusCode.UNAUTHORIZED)
-                .send('Incorrect password');
+            res.status(statusCodes_1.HttpStatusCode.UNAUTHORIZED).send('Incorrect password');
             return;
         }
-        const result = await userModel_1.default.deleteOne({ _id: userData._id });
-        console.log(result);
+        // Deleting user with specified _id
+        await userModel_1.default.deleteOne({ _id: userData._id });
         res.status(statusCodes_1.HttpStatusCode.NO_CONTENT).json();
     }
     catch (error) {
+        // MongoDB internal errors (deleteOne or findOne) or bcrypt comparison errors
         console.log(error);
-        res
-            .status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER)
-            .send('Internal server error');
+        res.status(statusCodes_1.HttpStatusCode.INTERNAL_SERVER).send('Internal server error');
     }
 };
 exports.deleteUser = deleteUser;
-// Searches database for user with email or name then return user data or null if user not found
+// Searches database for user with email | name then return user data or null if user not found
+// Errors must be handled outside the function (try/catch block)
 async function searchUser(userName, userEmail) {
     const userData = await userModel_1.default.findOne({
         $or: [
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,87 +1,87 @@
+// Importing express and hashing libraries
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 
+// Importing user validator schemas for validating requests 
 import { IGetUserValidatorSchema, ICreateUserValidatorSchema } from '../utils/validators/userValidator';
+//Importing user model and interface for mongoose validation
 import UserSchema, { IUserSchema } from "../models/userModel"
+// Importing HTTP status codes for response status codes
 import { HttpStatusCode } from '../config/statusCodes';
 
+// Function used to get user data from the database with email | name and password
 export const getUser = async (req: Request, res: Response): Promise<void> => {
+    // Casting body as IGetUserValidatorSchema interface and writing data to variables
+    // userName or userEmail will be provided
     const body = req.body as IGetUserValidatorSchema;
     const userName = body.name;
     const userEmail = body.email;
     const userPassword = body.password;
 
     try {
+        // Searches user by name or email and returns the user if found
         const userData = await searchUser(userName, userEmail);
         if (!userData) {
             res.status(HttpStatusCode.NOT_FOUND).send('User not found');
             return;
         }
 
-        // checks if the password is correct
+        // Checks if the password is correct
         const isMatch = await bcrypt.compare(userPassword, userData.password);
         if (!isMatch) {
-            res
-                .status(HttpStatusCode.UNAUTHORIZED)
-                .send('Incorrect password');
+            res.status(HttpStatusCode.UNAUTHORIZED).send('Incorrect password');
             return;
         }
 
+        // Creates a new userResponseData with user data that will be sent to the client
         const userResponseData = {
             name: userData.name,
             email: userData.email,
             signInDate: userData.signInDate,
         };
 
-        res
-            .status(HttpStatusCode.OK)
-            .json(userResponseData);
+        res.status(HttpStatusCode.OK).json(userResponseData);
     } catch (error) {
-        res
-            .status(HttpStatusCode.INTERNAL_SERVER)
-            .send('Internal server error');
+        // The errors caught during the searchUser execution (mongodb errors) or bcrypt comparison 
+        console.log(error);
+        res.status(HttpStatusCode.INTERNAL_SERVER).send('Internal server error');
     }
 }
 
+// Creates a new user with the specified data email, name and password if the user does not exist 
+// (name and email must not be in the db)
 export const createUser = async (req: Request, res: Response): Promise<void> => {
-    // save the request body to body variable with correct types 
+    // Casting body as ICreateUserValidatorSchema interface and writing data to variables
     const body = req.body as ICreateUserValidatorSchema;
-
     const userName = body.name;
     const userEmail = body.email;
     const userPassword = body.password;
 
-    // searches the database for the user
+    // Searches for user with the same email or name, if found responds to client
     try {
-        // if user was found then responds with conflict status and returns
         if (await searchUser(userName, userEmail)) {
-            res
-                .status(HttpStatusCode.CONFLICT)
-                .json({ message: 'User already exists' });
+            res.status(HttpStatusCode.CONFLICT).json({ message: 'User already exists' });
             return;
         }
     } catch (error) {
+        // Mongodb internal errors
         console.log(error);
-        res
-            .status(HttpStatusCode.INTERNAL_SERVER)
-            .json({ message: 'MongoDB error' });
+        res.status(HttpStatusCode.INTERNAL_SERVER).json({ message: 'MongoDB error' });
         return;
     }
 
-    // the hashing for the password
+    // The hashing for the password
     let hash: string;
     try {
-        // tries to hash the password using bcrypt
         hash = await bcrypt.hash(userPassword, 10);
     } catch (error) {
+        // Hashing password errors
         console.log(error);
-        res
-            .status(HttpStatusCode.INTERNAL_SERVER)
-            .json({ message: 'Hashing failed' });
+        res.status(HttpStatusCode.INTERNAL_SERVER).json({ message: 'Hashing failed' });
         return;
     }
 
-    // creates new user with hashed password and saves it to the database
+    // Creates new user with hashed password 
     const user = new UserSchema<IUserSchema>({
         name: userName,
         email: userEmail,
@@ -89,59 +89,54 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
         signInDate: new Date(),
     });
 
+    // Saving user to mongoDB database
     try {
         await user.save();
+        res.status(HttpStatusCode.OK).send(`User with name: ${userName} and email: ${userEmail} created successfully.`);
     }
     catch (error) {
+        // MongoDB internal errors
         console.log(error);
-        res
-            .status(HttpStatusCode.INTERNAL_SERVER)
-            .json({ message: 'User already exists' });
+        res.status(HttpStatusCode.INTERNAL_SERVER).json({ message: 'MongoDB error' });
         return;
     }
-
-    res
-        .status(HttpStatusCode.OK)
-        .send(`User with name: ${userName}, email: ${userEmail}, password: ${userPassword} created successfully.`);
 }
 
+// Removes user with the specified email | name and password if exists
 export const deleteUser = async (req: Request, res: Response): Promise<void> => {
+    // Casting body as IGetUserValidatorSchema interface and writing data to variables
     const body = req.body as IGetUserValidatorSchema;
-
     const userName = body.name;
     const userEmail = body.email;
     const userPassword = body.password;
 
     try {
+        // Searching for user in the db with userName or userEmail
         const userData = await searchUser(userName, userEmail);
         if (!userData) {
             res.status(HttpStatusCode.NOT_FOUND).send('User not found');
             return;
         }
 
-        // checks if the password is correct
+        // Checking if the password is correct
         const isMatch = await bcrypt.compare(userPassword, userData.password);
         if (!isMatch) {
-            res
-                .status(HttpStatusCode.UNAUTHORIZED)
-                .send('Incorrect password');
+            res.status(HttpStatusCode.UNAUTHORIZED).send('Incorrect password');
             return;
         }
 
-
-
-        const result = await UserSchema.deleteOne({ _id: userData._id });
-        console.log(result);
+        // Deleting user with specified _id
+        await UserSchema.deleteOne({ _id: userData._id });
         res.status(HttpStatusCode.NO_CONTENT).json();
     } catch (error) {
+        // MongoDB internal errors (deleteOne or findOne) or bcrypt comparison errors
         console.log(error);
-        res
-            .status(HttpStatusCode.INTERNAL_SERVER)
-            .send('Internal server error');
+        res.status(HttpStatusCode.INTERNAL_SERVER).send('Internal server error');
     }
 }
 
-// Searches database for user with email or name then return user data or null if user not found
+// Searches database for user with email | name then return user data or null if user not found
+// Errors must be handled outside the function (try/catch block)
 async function searchUser(userName?: string | undefined, userEmail?: string | undefined): Promise<IUserSchema | null> {
     const userData = await UserSchema.findOne({
         $or: [
