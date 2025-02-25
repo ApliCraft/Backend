cb8910be026a145205b8b206772ca86085a9123d
zaksiu12s <zaksiu279@gmail.com>
2025-02-25 11:03:45 +0100

    v0.3.0 add better getting recipes via more routes, and user authentication when getting private recipes

--- src/controllers/recipeController.ts
+++ src/controllers/recipeController.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express"
-import fs from 'fs/promises';
+import { Request, Response, NextFunction } from "express";
+import fs from "fs/promises";
 
 import RecipeSchema, { ImageType, RecipeType } from "../models/recipeModel";
 import { searchRecipes, searchRecipesPl } from "../services/recipeServices";
@@ -7,188 +7,276 @@ import { IAddRecipeValidatorSchema } from "../utils/validators/recipeValidator";
 import { searchProductById } from "../services/productServices";
 import { Types } from "mongoose";
 import User from "../models/userModel";
+import { verifyAccessToken } from "../utils/jwt";
 
 export const vectorSearchRecipe = async (_: Request, res: Response) => {
-    res.sendStatus(501);
-}
+  res.sendStatus(501);
+};
 
-export const getRecipe = async (req: Request, res: Response, next: NextFunction) => {
-    const { searchTerm, sendImages } = req.body;
+export const getRecipe = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { searchTerm, sendImages } = req.body;
 
+  try {
+    let recipes: (RecipeType & { base64Image?: string })[] =
+      await searchRecipes(searchTerm);
+
+    if (sendImages) {
+      console.log(sendImages);
+      recipes = await Promise.all(
+        recipes.map(
+          async (
+            recipe: RecipeType & { base64Image?: string }
+          ): Promise<RecipeType & { base64Image?: string }> => {
+            if (recipe.photo) {
+              const imagePath = recipe.photo.filePath;
+
+              try {
+                await fs.access(imagePath);
+
+                const imageBuffer = await fs.readFile(imagePath);
+                const base64Image = imageBuffer.toString("base64");
+                recipe.base64Image = base64Image;
+              } catch (err) {
+                // err if image does not exist
+              }
+            }
+
+            return recipe;
+          }
+        )
+      );
+    }
+
+    res.status(200).json(recipes);
+
+    return;
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const addRecipe = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const {
+    name,
+    prepareTime,
+    difficulty,
+    ingredients,
+    plName,
+    category,
+    author,
+    privacy,
+    preDescription,
+    description,
+    preparation,
+    keyWords,
+    base64Image,
+  } = req.body as IAddRecipeValidatorSchema;
+  let kcalPortion: number = 0;
+  let proteinPortion: number = 0;
+  let carbohydratesPortion: number = 0;
+  let fatContentPortion: number = 0;
+  let excludedDiets: string[] = [];
+  let allergens: string[] = [];
+
+  if (!Types.ObjectId.isValid(author)) {
+    res.status(400).json("Invalid author id.");
+    return;
+  }
+
+  const authorData = await User.findById(author);
+  if (!authorData) {
+    res.status(404).json("Author not found");
+    return;
+  }
+
+  // checks if user specified correct ingredient productId
+  ingredients.forEach((ingredient) => {
+    if (!Types.ObjectId.isValid(ingredient.productId)) {
+      res.status(400).json("Invalid MongoDB ObjectId");
+      return;
+    }
+  });
+
+  // name must be unique, this code ensures that it is unique
+  try {
+    const recipes: RecipeType[] = await searchRecipes(name);
+    if (recipes.length > 0) {
+      res.status(409).json(`Recipe with name ${name} already exists.`);
+      return;
+    }
+  } catch (err) {
+    next(err);
+    return;
+  }
+
+  // plName must be unique, this code ensures that it is unique
+  if (plName) {
     try {
-        let recipes: (RecipeType & { base64Image?: string })[] = await searchRecipes(searchTerm);
-
-        if (sendImages) {
-            console.log(sendImages);
-            recipes = await Promise.all(recipes.map(async (recipe: RecipeType & { base64Image?: string }): Promise<RecipeType & { base64Image?: string }> => {
-                if (recipe.photo) {
-                    const imagePath = recipe.photo.filePath;
-
-                    try {
-                        await fs.access(imagePath);
-
-                        const imageBuffer = await fs.readFile(imagePath);
-                        const base64Image = imageBuffer.toString('base64');
-                        recipe.base64Image = base64Image;
-                    } catch (err) {
-                        // err if image does not exist
-                    }
-                }
-
-                return recipe;
-            }));
-        }
-
-        res.status(200).json(recipes);
-
+      const products: RecipeType[] = await searchRecipesPl(plName);
+      if (products.length > 0) {
+        res.status(409).json(`Recipe with name ${plName} already exists.`);
         return;
+      }
     } catch (err) {
-        return next(err);
+      next(err);
+      return;
     }
-}
+  }
 
-export const addRecipe = async (req: Request, res: Response, next: NextFunction) => {
-    const { name, prepareTime, difficulty, ingredients, plName, category, author, privacy, preDescription, description, preparation, keyWords, base64Image } = req.body as IAddRecipeValidatorSchema;
-    let kcalPortion: number = 0;
-    let proteinPortion: number = 0;
-    let carbohydratesPortion: number = 0;
-    let fatContentPortion: number = 0;
-    let excludedDiets: string[] = [];
-    let allergens: string[] = [];
+  // checking privacy value
+  if (privacy !== "public" && privacy !== "private") {
+    res.status(400).json(`Privacy must be public or private.`);
+    return;
+  }
 
-    if (!Types.ObjectId.isValid(author)) {
-        res.status(400).json("Invalid author id.");
-        return;
-    }
+  // check if ingredients are empty
+  if (ingredients.length == 0) {
+    res.status(400).json(`Ingredients array cannot be empty.`);
+    return;
+  }
 
-    const authorData = await User.findById(author);
-    if (!authorData) {
-        res.status(404).json("Author not found");
-        return;
-    }
+  // checking if ingredients exist
+  const productErrors: string[] = [];
+  await Promise.all(
+    ingredients.map(async (ingredient) => {
+      const product = await searchProductById(ingredient.productId);
 
-    // checks if user specified correct ingredient productId
-    ingredients.forEach((ingredient) => {
-        if (!Types.ObjectId.isValid(ingredient.productId)) {
-            res.status(400).json('Invalid MongoDB ObjectId');
-            return;
-        }
+      if (!product) {
+        productErrors.push(
+          `Ingredient with id: ${ingredient.productId} doesn't exist.`
+        );
+      } else {
+        kcalPortion += product.kcalPortion * ingredient.quantity;
+        proteinPortion += product.proteinPortion * ingredient.quantity;
+        carbohydratesPortion +=
+          product.carbohydratesPortion * ingredient.quantity;
+        fatContentPortion += product.fatContentPortion * ingredient.quantity;
+
+        allergens = [...new Set([...allergens, ...product.allergens])];
+        excludedDiets = [
+          ...new Set([...excludedDiets, ...product.excludedDiets]),
+        ];
+      }
     })
+  );
 
-    // name must be unique, this code ensures that it is unique
+  if (productErrors.length > 0) {
+    res.status(404).json(productErrors);
+    return;
+  }
+
+  let photo: ImageType | undefined;
+  if (base64Image) {
     try {
-        const recipes: RecipeType[] = await searchRecipes(name);
-        if (recipes.length > 0) {
+      const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, "");
+      const buffer = Buffer.from(base64Data, "base64");
+      const fileName = `${name}-${Date.now()}.png`;
+      const filePath = `uploads/images/${fileName}`;
 
-            res.status(409).json(`Recipe with name ${name} already exists.`);
-            return
-        }
+      await fs.writeFile(filePath, buffer);
+      photo = {
+        fileName,
+        filePath,
+      };
     } catch (err) {
-        next(err);
-        return;
+      next(err);
+      return;
     }
+  }
 
-    // plName must be unique, this code ensures that it is unique
-    if (plName) {
-        try {
-            const products: RecipeType[] = await searchRecipesPl(plName);
-            if (products.length > 0) {
+  const newRecipe = new RecipeSchema({
+    photo,
+    plName,
+    prepareTime,
+    difficulty,
+    ingredients,
+    privacy,
+    preDescription,
+    name,
+    author: [],
+    likeQuantity: 0,
+    saveQuantity: 0,
+    description,
+    preparation,
+    keyWords: keyWords || [],
+    kcalPortion,
+    proteinPortion,
+    carbohydratesPortion,
+    fatContentPortion,
+    category,
+    excludedDiets: excludedDiets || [],
+    allergens: allergens || [],
+  });
 
-                res.status(409).json(`Recipe with name ${plName} already exists.`);
-                return
-            }
-        } catch (err) {
-            next(err);
-            return;
-        }
-    }
+  newRecipe.author.push(authorData);
 
-    // checking privacy value
-    if (privacy !== "public" && privacy !== "private") {
-        res.status(400).json(`Privacy must be public or private.`);
-        return
-    }
+  try {
+    await newRecipe.save();
 
-    // check if ingredients are empty
-    if (ingredients.length == 0) {
-        res.status(400).json(`Ingredients array cannot be empty.`);
-        return;
-    }
+    res.status(201).json(`Recipe with name ${name} created successfully.`);
+    return;
+  } catch (err) {
+    return next(err);
+  }
+};
 
-    // checking if ingredients exist
-    const productErrors: string[] = [];
-    await Promise.all(ingredients.map(async (ingredient) => {
-        const product = await searchProductById(ingredient.productId);
+export const getRecipeIdsByFilter = async (req: Request, res: Response) => {
+  const { category, from, limit, privacy } = req.body;
+  const token = req.headers.authorization?.split(" ")[1];
 
-        if (!product) {
-            productErrors.push(`Ingredient with id: ${ingredient.productId} doesn't exist.`);
+  if (privacy && !token) {
+    res.status(401).json("Token required to access private recipes");
+    return;
+  }
+
+  let query;
+  let queryConditions: any = { privacy: "public" };
+
+  if (token) {
+    try {
+      const decoded = verifyAccessToken(token);
+      if (decoded) {
+        const user = await User.findById(decoded.sub);
+        if (user) {
+          if (privacy === "private") {
+            queryConditions = {
+              privacy: "private",
+              author: user._id,
+            };
+          } else {
+            queryConditions = { privacy: "public" };
+          }
         } else {
-            kcalPortion += product.kcalPortion * ingredient.quantity;
-            proteinPortion += product.proteinPortion * ingredient.quantity;
-            carbohydratesPortion += product.carbohydratesPortion * ingredient.quantity;
-            fatContentPortion += product.fatContentPortion * ingredient.quantity;
-
-            allergens = [...new Set([...allergens, ...product.allergens])];
-            excludedDiets = [...new Set([...excludedDiets, ...product.excludedDiets])];
+          res.status(401).json("Unauthorized");
+          return;
         }
-    }));
-
-    if (productErrors.length > 0) {
-        res.status(404).json(productErrors);
-        return;
-    }
-
-    let photo: ImageType | undefined;
-    if (base64Image) {
-        try {
-            const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, '');
-            const buffer = Buffer.from(base64Data, 'base64');
-            const fileName = `${name}-${Date.now()}.png`;
-            const filePath = `uploads/images/${fileName}`;
-
-            await fs.writeFile(filePath, buffer);
-            photo = {
-                fileName,
-                filePath,
-            }
-        } catch (err) {
-            next(err);
-            return;
-        };
-    }
-
-    const newRecipe = new RecipeSchema({
-        photo,
-        plName,
-        prepareTime,
-        difficulty,
-        ingredients,
-        privacy,
-        preDescription,
-        name,
-        author: [],
-        likeQuantity: 0,
-        saveQuantity: 0,
-        description,
-        preparation,
-        keyWords: (keyWords || []),
-        kcalPortion,
-        proteinPortion,
-        carbohydratesPortion,
-        fatContentPortion,
-        category,
-        excludedDiets: (excludedDiets || []),
-        allergens: (allergens || []),
-    });
-
-    newRecipe.author.push(authorData);
-
-    try {
-        await newRecipe.save();
-
-        res.status(201).json(`Recipe with name ${name} created successfully.`);
-        return;
+      }
     } catch (err) {
-        return next(err);
+      console.log(err);
+      res.status(401).json("Unauthorized");
+      return;
     }
-}
+  }
+
+  if (category) {
+    queryConditions.category = category;
+  }
+
+  query = RecipeSchema.find(queryConditions, { _id: 1 }).lean().select("_id");
+
+  if (from !== undefined && limit !== undefined) {
+    query = query.skip(from).limit(limit);
+  }
+
+  const recipeIds = await query;
+
+  res.status(200).json(recipeIds);
+};
--- src/routes/v1/recipeRoutes.ts
+++ src/routes/v1/recipeRoutes.ts
@@ -1,140 +1,145 @@
-import { Router } from 'express';
-import { getRecipe, addRecipe, vectorSearchRecipe } from '../../controllers/recipeController';
-import { authenticateToken, validate } from '../../middleware/validate';
+import { Router } from "express";
+import {
+  getRecipe,
+  addRecipe,
+  vectorSearchRecipe,
+  getRecipeIdsByFilter,
+} from "../../controllers/recipeController";
+import { authenticateToken, validate } from "../../middleware/validate";
 
-import { AddRecipeValidatorSchema } from '../../utils/validators/recipeValidator';
-import { Types } from 'mongoose';
-import Recipe, { RecipeType } from '../../models/recipeModel';
-import fs from "fs/promises"
-import { searchRecipeById } from '../../services/recipeServices';
-import User from '../../models/userModel';
+import { AddRecipeValidatorSchema } from "../../utils/validators/recipeValidator";
+import { Types } from "mongoose";
+import Recipe, { RecipeType } from "../../models/recipeModel";
+import fs from "fs/promises";
+import { searchRecipeById } from "../../services/recipeServices";
+import User from "../../models/userModel";
 
 const router: Router = Router();
 
 router.get("/:id/update-like-count", authenticateToken, async (req, res) => {
-    const { id } = req.params;
-    const data = req.tokenData;
+  const { id } = req.params;
+  const data = req.tokenData;
 
-    if (!Types.ObjectId.isValid(id)) {
-        res.status(400).json('Invalid MongoDB ObjectId');
-        return;
-    }
+  if (!Types.ObjectId.isValid(id)) {
+    res.status(400).json("Invalid MongoDB ObjectId");
+    return;
+  }
 
-    console.log(data.sub)
-    const userData = await User.findById(data.sub);
-    if (!userData) {
-        res.status(404).json('User not found.');
-        return;
-    };
+  console.log(data.sub);
+  const userData = await User.findById(data.sub);
+  if (!userData) {
+    res.status(404).json("User not found.");
+    return;
+  }
 
+  const recipe = await Recipe.findById(id);
 
-    const recipe = await Recipe.findById(id);
+  if (!recipe) {
+    res.status(404).json("Recipe not found.");
+    return;
+  }
 
-    if (!recipe) {
-        res.status(404).json('Recipe not found.');
-        return;
-    }
+  if (
+    await User.findOne({
+      _id: data.sub,
+      likedRecipes: { $in: [recipe._id] },
+    })
+  ) {
+    res.status(409).json("Recipe already liked");
+    return;
+  }
 
-    if (await User.findOne({
-        _id: data.sub,
-        likedRecipes: { $in: [recipe._id] }
-    })) {
-        res.status(409).json("Recipe already liked");
-        return;
-    }
+  userData.likedRecipes.push(recipe);
+  userData.save();
 
+  recipe.likedBy.push(userData);
+  recipe.likeQuantity = recipe.likedBy.length;
+  recipe.save();
 
-    userData.likedRecipes.push(recipe);
-    userData.save();
-
-    recipe.likedBy.push(userData);
-    recipe.likeQuantity = recipe.likedBy.length;
-    recipe.save();
-
-
-    res.status(200).json("Updated like count.");
-})
+  res.status(200).json("Updated like count.");
+});
 
 router.get("/:id/delete-like-count", authenticateToken, async (req, res) => {
-    const { id } = req.params;
-    const data = req.tokenData;
+  const { id } = req.params;
+  const data = req.tokenData;
 
-    if (!Types.ObjectId.isValid(id)) {
-        res.status(400).json('Invalid MongoDB ObjectId');
-        return;
-    }
+  if (!Types.ObjectId.isValid(id)) {
+    res.status(400).json("Invalid MongoDB ObjectId");
+    return;
+  }
 
-    const userData = await User.findById(data.sub);
-    if (!userData) {
-        res.status(404).json('User not found.');
-        return;
-    };
+  const userData = await User.findById(data.sub);
+  if (!userData) {
+    res.status(404).json("User not found.");
+    return;
+  }
 
+  const recipe = await Recipe.findById(id);
+  if (!recipe) {
+    res.status(404).json("Recipe not found.");
+    return;
+  }
 
-    const recipe = await Recipe.findById(id);
-    if (!recipe) {
-        res.status(404).json('Recipe not found.');
-        return;
-    }
+  if (
+    !(await User.findOne({
+      _id: data.sub,
+      likedRecipes: { $in: [recipe._id] },
+    }))
+  ) {
+    res.status(409).json("Recipe not liked by this user");
+    return;
+  }
 
-    if (!(await User.findOne({
-        _id: data.sub,
-        likedRecipes: { $in: [recipe._id] }
-    }))) {
-        res.status(409).json("Recipe not liked by this user");
-        return;
-    }
+  userData.likedRecipes.remove(recipe);
+  userData.save();
 
+  recipe.likedBy.remove(userData);
+  recipe.likeQuantity = recipe.likedBy.length;
+  recipe.save();
 
-    userData.likedRecipes.remove(recipe);
-    userData.save();
-
-    recipe.likedBy.remove(userData);
-    recipe.likeQuantity = recipe.likedBy.length;
-    recipe.save();
-
-
-    res.status(200).json("Updated like count.");
-})
+  res.status(200).json("Updated like count.");
+});
 
 router.get("/:id", async (req, res, next) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if (!Types.ObjectId.isValid(id)) {
-        res.status(400).json('Invalid MongoDB ObjectId');
-        return;
+  if (!Types.ObjectId.isValid(id)) {
+    res.status(400).json("Invalid MongoDB ObjectId");
+    return;
+  }
+
+  try {
+    const recipe: (RecipeType & { base64Image?: string }) | null =
+      await searchRecipeById(id);
+
+    if (!recipe) {
+      res.status(404).json("Recipe not found.");
+      return;
     }
 
-    try {
-        const recipe: (RecipeType & { base64Image?: string }) | null = await searchRecipeById(id);
+    if (recipe.photo) {
+      const imagePath = recipe.photo.filePath;
 
-        if (!recipe) {
-            res.status(404).json('Recipe not found.');
-            return;
-        }
+      try {
+        await fs.access(imagePath);
 
-        if (recipe.photo) {
-            const imagePath = recipe.photo.filePath;
-
-            try {
-                await fs.access(imagePath);
-
-                const imageBuffer = await fs.readFile(imagePath);
-                const base64Image = imageBuffer.toString('base64');
-                recipe.base64Image = base64Image;
-            } catch (err) {
-                // err if image does not exist
-            }
-        }
-
-        res.status(200).json(recipe);
-    } catch (err) {
-        next(err);
-        return;
+        const imageBuffer = await fs.readFile(imagePath);
+        const base64Image = imageBuffer.toString("base64");
+        recipe.base64Image = base64Image;
+      } catch (err) {
+        // err if image does not exist
+      }
     }
+
+    res.status(200).json(recipe);
+  } catch (err) {
+    next(err);
+    return;
+  }
 });
 router.post("/get", getRecipe);
-router.post("/add", validate(AddRecipeValidatorSchema), addRecipe)
+router.post("/add", validate(AddRecipeValidatorSchema), addRecipe);
 router.post("/vector-search", vectorSearchRecipe);
+router.post("/get-by-category", getRecipeIdsByFilter);
 
 export default router;
