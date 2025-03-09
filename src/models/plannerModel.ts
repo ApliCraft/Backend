7fe1e5804635d0e69d95aa23abf678251e343edb
Maksymilian Zając <zaksiu279@gmail.com>
2025-03-09 14:41:57 +0100

    v0.9.0 add some basic planner functionality: adding meals and retrieving them :

---
+++ src/models/plannerModel.ts
@@ -0,0 +1,74 @@
+import mongoose, { InferSchemaType, model, Schema, Types } from "mongoose";
+
+const AdditionalProductSchema = new Schema({
+  _id: { type: Types.ObjectId, required: true, ref: "Product" },
+  amount: { type: Number, required: true },
+});
+
+const FluidEntrySchema = new Schema({
+  fluidId: { type: Types.ObjectId, required: true, ref: "Product" },
+  amount: { type: Number, required: true },
+  additional: { type: [AdditionalProductSchema], default: [] },
+});
+
+const ProductEntrySchema = new Schema({
+  productId: { type: Types.ObjectId, required: true, ref: "Product" },
+  amount: { type: Number, required: true },
+});
+
+const RecipeEntrySchema = new Schema({
+  recipeId: { type: Types.ObjectId, required: true, ref: "Recipe" },
+  portion: { type: Number, required: true, default: 1 },
+});
+
+const MealSchema = new Schema({
+  _id: { type: Types.ObjectId, auto: true, required: false },
+  category: { type: String, required: true },
+  time: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  products: { type: [ProductEntrySchema], default: [], required: false },
+  recipes: { type: [RecipeEntrySchema], default: [], required: false },
+});
+
+const SingleDaySchema = new Schema({
+  fluids: { type: [FluidEntrySchema], default: [] },
+  meals: { type: [MealSchema], default: [] },
+});
+
+const plannerSchema = new Schema(
+  {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      auto: true,
+    },
+    day: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    fluidIntakeAmount: {
+      required: true,
+      default: 2000,
+      type: Number,
+    },
+    planner: {
+      type: SingleDaySchema,
+      required: true,
+      default: {
+        fluids: [],
+        meals: [],
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+export type MealType = InferSchemaType<typeof MealSchema>;
+export type PlannerType = InferSchemaType<typeof plannerSchema>;
+const Planner = model<PlannerType>("Planner", plannerSchema);
+export default Planner;
--- src/routes/v1/userRoutes.ts
+++ src/routes/v1/userRoutes.ts
@@ -24,6 +24,8 @@ import User, { UserType } from "../../models/userModel";
 import mongoose, { isValidObjectId, Types } from "mongoose";
 import z from "zod";
 import Product from "../../models/productModel";
+import _ from "ollama/browser";
+import Planner, { MealType } from "../../models/plannerModel";
 
 const router: Router = Router();
 
@@ -451,6 +453,203 @@ router.put("/storage/increase-decrease-quantity", async (req, res) => {
     res.status(200).json("Product quantity updated successfully.");
   } catch {
     res.status(400).json("Invalid request body.");
+  }
+});
+
+const getCurrentDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // months are zero-indexed
+  const day = date.getDate().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
+
+const formatDate = (dateObj: any) => {
+  const year = dateObj.getFullYear();
+  const month = (dateObj.getMonth() + 1).toString().padStart(2, "0"); // months are zero-indexed
+  const day = dateObj.getDate().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
+
+const addMealSchema = z.object({
+  date: z.coerce.date(),
+  type: z.enum(["product", "recipe"]),
+  _id: z.string(), // product or recipe valid mongodb _id
+  category: z.string(),
+  portion: z.number(), // if product => amount, if recipe => portion
+  completed: z.boolean().default(false),
+});
+
+// @ts-ignore
+const getHourAndMinutes = (dateObj) => {
+  const hours = dateObj.getHours().toString().padStart(2, "0"); // Get the hours and pad with leading zero if necessary
+  const minutes = dateObj.getMinutes().toString().padStart(2, "0"); // Get the minutes and pad with leading zero if necessary
+
+  return `${hours}:${minutes}`;
+};
+
+router.post("/planner/add-meal", async (req, res) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      res.status(400).json("No token provided");
+      return;
+    }
+
+    const decoded = verifyAccessToken(token);
+    if (!decoded) {
+      res.status(401).json("Invalid token.");
+      return;
+    }
+
+    const user = await User.findById(decoded.sub);
+
+    if (!user) {
+      res.status(404).json("User not found.");
+      return;
+    }
+
+    let parsed;
+    try {
+      parsed = addMealSchema.parse(req.body);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json("Some data is missing in your request");
+      return;
+    }
+
+    const { _id } = parsed;
+
+    if (!isValidObjectId(_id)) {
+      res.status(400).json("Not correct _id.");
+      return;
+    }
+
+    const { date, category, completed, type, portion } = parsed;
+
+    const dateString = formatDate(date);
+
+    const planner = await Planner.findOne({
+      userId: user._id,
+      day: dateString,
+    });
+
+    if (!planner) {
+      const newPlanner = new Planner({
+        day: dateString,
+        userId: user._id,
+      });
+
+      const newMeal: MealType = {
+        category,
+        time: getHourAndMinutes(date),
+        completed,
+      };
+
+      if (type == "product") {
+        // @ts-ignore
+        newMeal.products = [{ productId: _id, amount: portion }];
+      }
+
+      if (type == "recipe") {
+        // @ts-ignore
+        newMeal.recipes = [{ recipeId: _id, portion: portion }];
+      }
+
+      newPlanner.planner.meals.push(newMeal);
+
+      await newPlanner.save();
+
+      res.status(200).json("Planner created");
+      return;
+    }
+
+    const newMeal: MealType = {
+      category,
+      time: getHourAndMinutes(date),
+      completed,
+    };
+
+    if (type == "product") {
+      // @ts-ignore
+      newMeal.products = [{ productId: _id, amount: portion }];
+    }
+
+    if (type == "recipe") {
+      // @ts-ignore
+      newMeal.recipes = [{ recipeId: _id, portion: portion }];
+    }
+
+    planner.planner.meals.push(newMeal);
+
+    await planner.save();
+
+    res.status(200).json(dateString);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    return;
+  }
+});
+
+const mealsSchema = z.object({
+  date: z.coerce.date(),
+});
+
+router.post("/planner/meals", async (req, res) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      res.status(400).json("No token provided");
+      return;
+    }
+
+    const decoded = verifyAccessToken(token);
+    if (!decoded) {
+      res.status(401).json("Invalid token.");
+      return;
+    }
+
+    const user = await User.findById(decoded.sub);
+
+    if (!user) {
+      res.status(404).json("User not found.");
+      return;
+    }
+
+    let parsed;
+    try {
+      parsed = mealsSchema.parse(req.body);
+    } catch {
+      res.status(400).json("not all data specified in request");
+      return;
+    }
+
+    const { date } = parsed;
+
+    const dateString = formatDate(date);
+
+    const planner = await Planner.findOne({
+      userId: user._id,
+      day: dateString,
+    }).lean();
+
+    if (!planner) {
+      res
+        .status(404)
+        .json("Planner doesn't exist for this day, add some products");
+      return;
+    }
+
+    res.status(200).json(planner);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json();
+    return;
   }
 });
 
