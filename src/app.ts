68486be2dab3acd26e70d91eb60e3bc0862d426d
Maksymilian Zając <zaksiu279@gmail.com>
2025-03-12 00:12:06 +0100

    add option to retrieve synchrous data from llmaa

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import express, { Application, Response } from "express";
 import cors from "cors";
-import connectToMongoDB from './config/db';
+import connectToMongoDB from "./config/db";
 import { HttpStatusCode } from "./config/statusCodes";
 
 // Import routes
-import userRoutes from './routes/v1/userRoutes';
-import recipeRoutes from './routes/v1/recipeRoutes';
-import productRoutes from './routes/v1/productRoutes';
-import { errorHandler } from './middleware/errors';
+import userRoutes from "./routes/v1/userRoutes";
+import recipeRoutes from "./routes/v1/recipeRoutes";
+import productRoutes from "./routes/v1/productRoutes";
+import { errorHandler } from "./middleware/errors";
 import useragent from "express-useragent";
 import imageRoutes from "./routes/v1/imageRoutes";
-
+import { streamChatWithOllama } from "./services/ollama.service";
 
 const app: Application = express();
 // let isConnectedToMongoDB = false;
@@ -31,17 +31,40 @@ app.use(express.urlencoded({ extended: false }));
 
 // Use the imported routes
 app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/recipe", recipeRoutes)
+app.use("/api/v1/recipe", recipeRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use("/api/v1/images", imageRoutes)
+app.use("/api/v1/images", imageRoutes);
 
 // Error handling middleware
 app.use(errorHandler);
 
-app.get('/api', (_, res: Response): void => {
+app.post("/api/v1/llama/chat", async (req, res) => {
+  try {
+    const { message } = req.body;
+
+    if (!message) {
+      res.status(400).json("no message");
+      return;
+    }
+
+    const response = streamChatWithOllama("write max of 200 words" + message);
+    let responseText = "";
+
+    for await (const part of response) {
+      responseText += part;
+    }
+
+    res.status(200).json(responseText);
+  } catch {
+    res.status(500);
+    return;
+  }
+});
+
+app.get("/api", (_, res: Response): void => {
   res.status(HttpStatusCode.OK).json({
-    info: 'API running...',
-    api: ["/api/v1/users", "/api/v1/recipe", "/api/v1/product"]
+    info: "API running...",
+    api: ["/api/v1/users", "/api/v1/recipe", "/api/v1/product"],
   });
 });
 
